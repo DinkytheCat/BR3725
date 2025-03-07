44c95a17ddcb86a6a761ff018813878e80263fde
DinkytheCat <157430242+DinkytheCat@users.noreply.github.com>
2025-03-07 15:12:28 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -3,13 +3,13 @@ def multiply(n, m):
     shift = 0   
     
     while m > 0:
-        if m & 1 == 1:
-            asnwer = asnwer + (n << shift) 
-        m = m >> 1
+        if m & 1 == 1: #checks if the least bit is at least 1 to continue
+            asnwer = asnwer + (n << shift) #adds the shifted nmuber n to the new answer
+        m = m >> 1 #right shift m by 1 whcih divides it by 2
         shift = shift + 1
     return asnwer
     
-n = int(input("n"))
+n = int(input("n")) #makes it as an integer
 m = int(input("m"))
 
 print(multiply(n, m))
